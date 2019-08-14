5da49e1a4541ee3a0d64135405ac103c235fe602
prathamghadge <prathmesh071@gmail.com>
2019-08-14 20:55:33 +0530

    test change

--- src/main/java/com/sap/docker/MainServlet.java
+++ src/main/java/com/sap/docker/MainServlet.java
@@ -16,7 +16,7 @@ public class MainServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String name = req.getParameter("name");
-        resp.getWriter().write("Hello " + name);
+        resp.getWriter().write("Hi" + name);
         resp.getWriter().close();
     }
 }
